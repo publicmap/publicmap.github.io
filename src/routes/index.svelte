0990b157483ed5b7535972914a9d9d98d4ff9f27
planemad <arun.planemad@gmail.com>
2020-11-25 02:23:05 -0500

    Remove css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,56 +1,24 @@
 <script>
-	import successkid from 'images/successkid.jpg';
-	import Map from "../components/Map.svelte";
+  import Map from "../components/Map.svelte";
+  import Panel from "../components/Panel.svelte";
 
-	let map;
+  let map;
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
-	<title>Open Atlas</title>
+  <title>Open Atlas</title>
 </svelte:head>
 
-<h1>A!</h1>
+<Panel>
+  <div id="panel" class="uk-position-absolute uk-padding-small uk-width-1-1">
+    <h1>A!</h1>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
+    <p>Hi
+    </p>
+  </div>
+</Panel>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
-
-<Map
-  bind:map />
+<Map bind:map />
