48aa42032ea53c07da5c897c5c7678a0aa82f1a3
planemad <arun.planemad@gmail.com>
2020-11-25 02:25:40 -0500

    Add panel

---
+++ src/components/Panel.svelte
@@ -0,0 +1,18 @@
+<style>
+	div {
+		z-index: 2;
+		width: 40%;
+		position: absolute;
+	}
+	@media (max-width: 1000px) {
+		div {
+			width: 100%;
+			top: 60%;
+		}
+	}
+
+</style>
+
+<div>
+	<slot></slot>
+</div>
