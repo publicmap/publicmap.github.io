4defbbd940d7a78ad1b509090b4bf4e1393b931b
planemad <arun.planemad@gmail.com>
2020-11-25 02:28:45 -0500

    Remove / from basepath

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const dev = NODE_ENV === "development";
 
 polka() // You can also use Express
   .use(
-    "/open-atlas",
+    "open-atlas",
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
